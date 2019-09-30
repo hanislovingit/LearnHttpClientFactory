2e01714230afd3ca07209cb7dbcc5f08297eb796
Chuck Han <chuck.han@proagrica.com>
2019-09-29 21:56:29 -0500

    trying a different way to set up polly

--- HttpClientFactory/DelegatingHandlers/PollyRetryDelegatingHandler.cs
+++ HttpClientFactory/DelegatingHandlers/PollyRetryDelegatingHandler.cs
@@ -17,12 +17,23 @@ namespace HttpClientFactory.DelegatingHandlers
 
         protected override Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request,
-            CancellationToken cancellationToken) =>
-            Policy
-                .Handle<HttpRequestException>()
-                .Or<TaskCanceledException>()
-                .OrResult<HttpResponseMessage>(x => !x.IsSuccessStatusCode)
-                .RetryAsync(_numberOfRetries)
-                .ExecuteAsync(() => base.SendAsync(request, cancellationToken));
+            CancellationToken cancellationToken)
+        {
+            // There are different options to set up polly
+
+            // option 1
+            //return Policy
+            //    .Handle<HttpRequestException>()
+            //    .Or<TaskCanceledException>()
+            //    .OrResult<HttpResponseMessage>(x => !x.IsSuccessStatusCode)
+            //    .RetryAsync(_numberOfRetries)
+            //    .ExecuteAsync(() => base.SendAsync(request, cancellationToken));
+
+            // option 2
+            var retryPolicy = Policy.HandleResult<HttpResponseMessage>(x => !x.IsSuccessStatusCode)
+                .RetryAsync(_numberOfRetries);
+            return retryPolicy.ExecuteAsync(() => base.SendAsync(request, cancellationToken));
+        }
+            
     }
 }
